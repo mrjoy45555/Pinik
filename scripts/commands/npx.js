0010e7682e2ddcfd3ff6ff77c841f82563deec19
mrjoy45555 <joyahmed56joy@gmail.com>
2025-06-15 11:20:54 +0600

    Create npx.js

---
+++ scripts/commands/npx.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "💦",
+  version: "1.0.0", 
+  permission: 0,
+  credits: "Joy",
+  description: "", 
+  prefix: true,
+  category: "user",
+  usages: "",
+  cooldowns: 5, 
+  
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("bristi")==0 || event.body.indexOf("Bristi")==0 || event.body.indexOf("BRISTI")==0 || event.body.indexOf("bristi")==0) {
+		var msg = {
+				body: "-𝗝𝗢𝗬 𝗔𝗛𝗠𝗘𝗗 😻",
+				attachment: fs.createReadStream(__dirname + `/Joy/JOY12.mp4`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("😇", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
