e6fe79e9507cb25d58aa2d53582c5e3d0fa2f225
mrjoy45555 <joyahmed56joy@gmail.com>
2025-06-15 12:05:36 +0600

    Update npx.js

--- scripts/commands/npx.js
+++ scripts/commands/npx.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "💦",
+  name: "npx",
   version: "1.0.0", 
   permission: 0,
   credits: "Joy",
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	if (event.body.indexOf("bristi")==0 || event.body.indexOf("Bristi")==0 || event.body.indexOf("BRISTI")==0 || event.body.indexOf("bristi")==0) {
 		var msg = {
 				body: "-𝗝𝗢𝗬 𝗔𝗛𝗠𝗘𝗗 😻",
-				attachment: fs.createReadStream(__dirname + `/Joy/JOY12.mp4`)
+				attachment: fs.createReadStream(__dirname + `/Joy/JOY12.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
     api.setMessageReaction("😇", event.messageID, (err) => {}, true)
