3249fd2938ca0d362d2bd35f0143612f6cec8aab
mrjoy45555 <joyahmed56joy@gmail.com>
2025-06-18 17:16:49 +0600

    Create gift.js

---
+++ scripts/commands/gift.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+
+module.exports = {
+  config: {
+    name: "gift",
+    version: "1.1",
+    hasPermssion: 2,
+    credits: "Mostakim", // 🔒 Don't change this
+    description: "bom attack",
+    usage: "[count]",
+    commandCategory: "tools",
+    cooldowns: 5,
+  },
+
+  run: async function({ api, event, args }) {
+    const { threadID, messageID } = event;
+
+    
+    if (module.exports.config.credits !== "Mostakim") {
+      return api.sendMessage("❌ Pok ।", threadID, messageID);
+    }
+
+    let count = parseInt(args[0]);
+    if (isNaN(count) || count <= 0) {
+      return api.sendMessage("⚠️ সঠিকভাবে সংখ্যা দাও যেমন: bom 10", threadID, messageID);
+    }
+
+    if (count > 50) count = 50;
+
+    const urls = [
+      "https://raw.githubusercontent.com/Alifhosson/ALIF-BOT.json/refs/heads/main/bom2.json",
+      "https://raw.githubusercontent.com/dipto-008/D1PT0/refs/heads/main/bom.json"
+    ];
+
+    let success = 0, fail = 0;
+
+    for (let i = 0; i < count; i++) {
+      const url = urls[Math.floor(Math.random() * urls.length)];
+      try {
+        await axios.get(url);
+        success++;
+      } catch (e) {
+        fail++;
+      }
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+
+    return api.sendMessage(`✅ বোমিং সম্পন্ন\nসফল: ${success}\nব্যর্থ: ${fail}`, threadID, messageID);
+  }
+};
